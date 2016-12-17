87cc9eb342039fd62565b73f5f37dda7aa9fa4db
Görkem Ziya Yanık <yanikgo@itu.edu.tr>
2016-12-17 12:45:42 +0200

    Update TempCond.c

--- TempCond.c
+++ TempCond.c
@@ -28,7 +28,7 @@ int main(){
 	else if( pe2a_getTemperature(1) < MinTemp){
 
 		 printf("pe2a_getTemperature(1) : %.2f \n",pe2a_getTemperature(1));
-		 pe2a_DO_setHigh(pe2a_GPIO_J4_1);
+		 pe2a_DO_setHigh(pe2a_GPIO_J4_3);
 
 	}
 	
